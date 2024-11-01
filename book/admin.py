6958dca5b76a2bce7bb272e5edc61dbaecec6275
abhidharsh <abhidharsh1999@gmail.com>
2024-11-01 15:36:13 +0530

    completed

--- book/admin.py
+++ book/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from . import models
+from django.contrib.auth.models import Group, User
 
 # Register your models here.
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
 
 
 @admin.register(models.Users)
